360baecf2af54ca48b3a89e605de203b878de30d
dnslin <i@dnsl.in>
2026-05-07 03:59:33 +0000

    fix(illustrations): 修正 EmptyState 导出为命名导出

--- src/components/illustrations/index.ts
+++ src/components/illustrations/index.ts
@@ -1,3 +1,3 @@
 export { default as MoodIcon } from './MoodIcons'
-export { default as EmptyState } from './EmptyState'
+export { EmptyState } from './EmptyState'
 export { default as CoverDecorations } from './CoverDecorations'
